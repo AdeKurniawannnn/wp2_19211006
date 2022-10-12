e30498c40cb91c42a9d8d74d61d88f56b860ae26
AdeKurniawannnn <adekurniawankebi2@gmail.com>
2022-10-12 17:47:42 +0700

    Penambahan validasi ade 19211006

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -18,32 +18,58 @@
                         <hr>
                     </td>
                 </tr>
+
                 <tr>
                     <th>Kode MTK</th>
                     <th>:</th>
                     <td>
-                        <input type="text" name="kode" id="kode">
+                        <input type="text" name="kode" id="kode" value="<?= set_value('kode') ?>">
                     </td>
                 </tr>
                 <tr>
-                    <th>Nama MTK</th>
-                    <td>:</td>
+                    <th></th>
+                    <th></th>
                     <td>
-                        <input type="text" name="nama" id="nama">
+                        <font style="color:red; font-size:8; align:center;"><?= form_error('kode'); ?></font>
                     </td>
+                <tr>
+
+                </tr>
+                <th>Nama MTK</th>
+                <th>:</th>
+                <td>
+                    <input type=" text" name="nama" id="nama" value="<?= set_value('nama') ?>">
+                </td>
                 </tr>
                 <tr>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <td>
+                        <font style="color:red; font-size:8; align:center;"><?= form_error('nama'); ?></font>
+                    </td>
+                <tr>
+
                     <th>SKS</th>
                     <td>:</td>
                     <td>
                         <select name="sks" id="sks">
-                            <option value="">Pilih SKS</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
+                            <option value="" <?php echo set_select('sks', 'Pilih SKS', TRUE); ?>Pilih SKS</option>
+                            <option value="2" <?php echo set_select('sks', '2'); ?>>2</option>
+                            <option value="3" <?php echo set_select('sks', '2'); ?>>3</option>
+                            <option value="4" <?php echo set_select('sks', '2'); ?>>2</option>
                         </select>
                     </td>
                 </tr>
+                <tr>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <td>
+                        <font style="color:red; font-size:8; align:center;"><?= form_error('sks'); ?></font>
+
+                    </td>
+                <tr>
                 <tr>
                     <td colspan="3" align="center">
                         <input type="submit" value="Submit">
